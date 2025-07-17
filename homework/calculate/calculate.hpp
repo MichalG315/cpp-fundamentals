cdda26f5235c654eb2f4d4205f3c144ba9ee9afe
MichalG315 <mgrot315@gmail.com>
2025-07-17 21:45:01 +0200

    Invalid data works

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -2,6 +2,21 @@
 #include <string>
 
 std::string calculate(const std::string& command, int first, int second) {
-    // TODO: Implement your solution here and return proper value
-    return "";
+    if (command == "add") {
+        return "";
+    }
+
+    else if (command == "substract") {
+        return "";
+    }
+
+    else if (command == "multiply") {
+        return "";
+    }
+
+    else if (command == "divide") {
+        return "";
+    } else {
+        return "Invalid data";
+    }
 }
