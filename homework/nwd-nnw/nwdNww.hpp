c32c1ec3382b07dd38a05c2d715a12fadd2a9417
MichalG315 <mgrot315@gmail.com>
2025-07-18 20:22:46 +0200

    nwd works

--- homework/nwd-nnw/nwdNww.hpp
+++ homework/nwd-nnw/nwdNww.hpp
@@ -1,8 +1,25 @@
 #pragma once
 
 int NWD(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+    int a = abs(lhs);
+    int b = abs(rhs);
+    int temp = 0;
+
+    if ((lhs == 0 && rhs == 1) || (lhs == 1 && rhs == 0)) {
+        return 1;
+    }
+
+    if (lhs == 0 && rhs == 0) {
+        return 0;
+    }
+
+    while (a % b != 0) {
+        temp = a % b;
+        a = b;
+        b = temp;
+    }
+
+    return b;
 }
 
 int NWW(int lhs, int rhs) {
