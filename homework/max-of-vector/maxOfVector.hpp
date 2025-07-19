be70af159cf1bdbcac0ba7655b6c6a9d59da4f61
MichalG315 <mgrot315@gmail.com>
2025-07-19 13:41:19 +0200

    maxOfVector works

--- homework/max-of-vector/maxOfVector.hpp
+++ homework/max-of-vector/maxOfVector.hpp
@@ -1,8 +1,13 @@
 #pragma once
+#include <algorithm>
 #include <limits>
 #include <vector>
 
 int maxOfVector(const std::vector<int>& vec) {
-    // TODO: Implement me :)
-    return {};
+    std::vector<int> vector = vec;
+    std::sort(vector.begin(), vector.end());
+
+    int max = vector.back();
+
+    return max;
 }
